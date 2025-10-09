b7cb4a75c911b6fed55cef290da6f7b879f854f3
ivy-jh <jens.hartmann@axonivy.com>
2025-10-09 14:37:00 +0200

    wip

--- org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/utils/MarkdownConverter.java
+++ org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/utils/MarkdownConverter.java
@@ -15,13 +15,9 @@ package org.eclipse.lemminx.utils;
 import static org.apache.commons.lang3.StringEscapeUtils.unescapeJava;
 import static org.apache.commons.lang3.StringEscapeUtils.unescapeXml;
 
-import java.lang.reflect.Field;
 import java.util.logging.Logger;
 
-import com.overzealous.remark.Options;
-import com.overzealous.remark.Options.FencedCodeBlocks;
-import com.overzealous.remark.Options.Tables;
-import com.overzealous.remark.Remark;
+import com.vladsch.flexmark.html2md.converter.FlexmarkHtmlConverter;
 
 import org.jsoup.safety.Cleaner;
 import org.jsoup.safety.Safelist;
@@ -35,44 +31,17 @@ public class MarkdownConverter {
 
 	private static final Logger LOGGER = Logger.getLogger(MarkdownConverter.class.getName());
 
-	private static Remark remark;
+	private static final FlexmarkHtmlConverter converter = FlexmarkHtmlConverter.builder().build();
 
 	private MarkdownConverter(){
 		//no public instanciation
-	}
-
-	static {
-		Options options = new Options();
-		options.tables = Tables.CONVERT_TO_CODE_BLOCK;
-		options.hardwraps = true;
-		options.inlineLinks = true;
-		options.autoLinks = true;
-		options.reverseHtmlSmartPunctuation = true;
-		options.fencedCodeBlocks = FencedCodeBlocks.ENABLED_BACKTICK;
-		remark = new Remark(options);
-		//Stop remark from stripping file protocol in an href
-		try {
-			Field cleanerField = Remark.class.getDeclaredField("cleaner");
-			cleanerField.setAccessible(true);
-
-			Cleaner c = (Cleaner) cleanerField.get(remark);
-
-			Field safelistField = Cleaner.class.getDeclaredField("safelist");
-			safelistField.setAccessible(true);
-
-			Safelist s = (Safelist) safelistField.get(c);
-
-			s.addProtocols("a", "href", "file");
-		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-			LOGGER.severe("Unable to modify jsoup to include file protocols "+ e.getMessage());
-		}
 	}
 
 	public static String convert(String html) {
 		if(!StringUtils.isTagOutsideOfBackticks(html)) {
 			return unescapeXml(html); // is not html so it can be returned as is (aside from unescaping)
 		}
-		return unescapeJava(remark.convert(html));
+		return unescapeJava(converter.convert(html));
 	}
 
 }
